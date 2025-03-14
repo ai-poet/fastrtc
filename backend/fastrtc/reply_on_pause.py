728b79f98ecc61709dc148211fb29a1ca876e793
huzw1995 <huzw1995@gmail.com>
2025-03-15 00:03:30 +0800

    fix: 添加开始说话事件

--- backend/fastrtc/reply_on_pause.py
+++ backend/fastrtc/reply_on_pause.py
@@ -149,6 +149,7 @@ class ReplyOnPause(StreamHandler):
             ):
                 state.started_talking = True
                 logger.debug("Started talking")
+                self.send_message_sync(create_message("log", "started_talking"))
                 self.interrupt_response()
             if state.started_talking:
                 if state.stream is None:
